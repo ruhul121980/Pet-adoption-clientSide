e73d9ef7efb39ae4168dc89ba7f89a25d67522eb
unknown <ruhul121980@gmail.com>
2024-05-14 01:13:10 +0600

    basic layout

---
+++ src/components/home/PetAccessoriesSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+ 
+function PetAccessoriesSection() {
+  return (
+    <div className='min-h-screen'>
+        PetAccessoriesSection
+    </div>
+  );
+}
+
+export default PetAccessoriesSection;
---
+++ src/components/home/VeterinarianSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+ 
+function VeterinarianSection() {
+  return (
+    <div>
+        VeterinarianSection
+    </div>
+  );
+}
+
+export default VeterinarianSection;
